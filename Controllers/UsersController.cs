99a5448052163ace25982d84987cb81541f88976
Theflowz0ne <theflowzon3@gmail.com>
2022-01-23 15:16:13 +0200

    login form

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVRWebsite_Unibit_5sem_MWT_MVC.Models;
+
+namespace MVRWebsite_Unibit_5sem_MWT_MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(User user)
+        {
+            using (MVR_WEBSITEEntities db = new MVR_WEBSITEEntities())
+            {
+                User existingUser = db.User.Where(u => u.Username == user.Username && u.Password == user.Password).FirstOrDefault();
+                if(existingUser != null)
+                {
+                    Session["username"] = existingUser.Username;
+                    Session["role"] = existingUser.Role;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View("Login");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout(User user)
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Users");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVRWebsite_Unibit_5sem_MWT_MVC.MvcApplication" Language="C#" %>
